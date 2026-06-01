e52b36d5b67f6112367ab2e96182245ca306b143
Samuel Mendoza <contacto@sdstico.com>
2026-06-01 11:15:13 -0500

    Backup del servidor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,22 +2,10 @@ module.exports = {
   apps: [
     {
       name: "wooposadmin",
-      interpreter: "/opt/wooposadmin/.venv/bin/python",
-      script: "/opt/wooposadmin/.venv/bin/streamlit",
-      args: "run app_web.py",
-      cwd: "/opt/wooposadmin",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "700M",
-      env: {
-        STREAMLIT_SERVER_HEADLESS: "true",
-        STREAMLIT_SERVER_PORT: "8501",
-        STREAMLIT_SERVER_ADDRESS: "127.0.0.1",
-        TZ: "America/Bogota",
-      },
-      error_file: "/var/log/wooposadmin/error.log",
-      out_file: "/var/log/wooposadmin/out.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-    },
-  ],
-};
+      script: "/usr/projects/dyoapp/dyoapp/.venv/bin/streamlit",
+      args: "run app_web.py --server.port 8501 --server.address 0.0.0.0",
+      cwd: "/usr/projects/dyoapp/dyoapp",
+      interpreter: "none"
+    }
+  ]
+}
